f88942cfec43d08a892791d0516b67735bcfea5f
xuchunhao <1010549831@qq.com>
2019-02-08 14:58:03 +0800

    add register-box and login-box

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var registerClick = document.getElementsByClassName('register-click');
+var register = document.getElementsByClassName('register')[0];
+var loginClick = document.getElementsByClassName('login-click');
+var login = document.getElementsByClassName('login')[0];
+
+function registerClickEvent() {
+    for (var i = 0; i < registerClick.length; i++) {
+        registerClick[i].onclick = function() {
+            register.style.display = "block";
+        }
+    }
+
+}
+
+function loginClickEvent() {
+    for (var i = 0; i < loginClick.length; i++) {
+        loginClick[i].onclick = function() {
+            login.style.display = "block";
+        }
+    }
+}
+
+function delRegister() {
+    var del = document.getElementsByClassName('del');
+    for (var i = 0; i < del.length; i++) {
+        del[i].onclick = function() {
+            register.style.display = "none";
+            login.style.display = "none";
+        }
+    }
+}
+registerClickEvent();
+loginClickEvent();
+delRegister();
