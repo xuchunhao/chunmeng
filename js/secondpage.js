25e6b1e50620e67584cab8287269656b4e695ba8
xuchunhao <1010549831@qq.com>
2019-02-18 00:28:05 +0800

    add something

--- js/secondpage.js
+++ js/secondpage.js
@@ -4,3 +4,12 @@ var n2 = loc.indexOf('=');
 var txt = decodeURI(loc.substr(n2 + 1, n1 - n2));
 var box = document.getElementsByTagName('dt')[0];
 box.innerHTML = txt;
+
+
+$('dd').click(function() {
+    $('dd').removeClass('active');
+    $(this).addClass('active');
+    $('.right-content div').removeClass('show');
+    var dataId = $(this).attr('data-id');
+    $("." + dataId).addClass('show');
+});
